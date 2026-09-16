52f2da31695f5a57cfd6267862099f1ddb43a476
shreedharsnara <114737785+shreedharsnara@users.noreply.github.com>
2026-09-17 00:19:12 +0530

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="citibike_etl",
-    version="0.0.1 ",
+    version="0.0.2 ",
     description="This contains the code in the ./src directory of the project",
     author="Shreedhar Nara",
     packages=find_packages(where="./src"),
